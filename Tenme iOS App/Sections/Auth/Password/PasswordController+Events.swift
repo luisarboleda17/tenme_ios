0c85aa8a1a0ae305ddf104779eba68de860cd04f
luis_arboleda17 <louisaurios@gmail.com>
2019-07-14 13:28:24 -0500

    Password view UI improved

--- Tenme iOS App/Sections/Auth/Password/PasswordController+Events.swift
+++ Tenme iOS App/Sections/Auth/Password/PasswordController+Events.swift
@@ -2,16 +2,14 @@
 //  PasswordController+Events.swift
 //  Tenme iOS App
 //
-//  Created by Luis Arboleda on 7/3/19.
+//  Created by Luis Arboleda on 7/14/19.
 //  Copyright © 2019 Tenme. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 extension PasswordController {
-    @IBAction func signBtnTouched(_ sender: Any) {
-        if let password = self.passwordTxt?.text {
-            self.viewModel.signIn(password: password)
-        }
+    @IBAction func continueSignIn(_ sender: Any) {
+        signIn()
     }
 }
